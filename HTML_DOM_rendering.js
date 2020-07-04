2b49cc99c617ebea92db6115052a56a6a183fe92
Christian B <christianbear1221@gmail.com>
2020-07-03 18:20:07 -0700

    rendering & css

--- HTML_DOM_rendering.js
+++ HTML_DOM_rendering.js
@@ -1,19 +1,24 @@
-window.onload = ()=>{
+window.onload = async ()=>{
     var setup = new MountMe();
     setup.make('#urlText', 'URL Buddy', "");
     setup.addClass('#urlText', 'header');
 
 
     var foot = new MountMe();
-    foot.make('#footMain', '', '#urlText');
+    foot.make('#footMain', '', '#urlText');             //main attachment to body
     //TODO - make button 'divs' to click => hide/show
-    foot.make('#footSub', '1', '');     
-    foot.addClass('#footSub', 'footer');
+    foot.make('#footSub', '', '#footMain');             // sub foot
+    foot.addClass('#footSub', 'footer');                // add footer style
+    foot.make('#footSubDivEnd', '', '#footSub');        // sub sub foot
+    foot.addClass('#footSubDivEnd', 'gridFooter');      // add grid style
+    foot.make('#footSub01', 'New Message', '#footSubDivEnd');   //item 1
+    foot.make('#footSub02', 'Outbox', '#footSubDivEnd');        //item 2
+    foot.make('#footSub03', 'Inbox', '#footSubDivEnd');         //item 3
 
     makeNewMessageGUI();
 
-
-   
+    
+    await clickable();        //addEventListener(s) for footer
 
 
 
@@ -73,9 +78,37 @@ function makeNewMessageGUI(){
 
 function makeOutboxGUI(){
     var outbox = new outbox();
-    outbox.getMessagesFromOutbox();
+    outbox.getMessagesFro
+    
+    mOutbox();
 
 
+}
+
+
+//========================================
+
+function makeInboxGUI(){
+
+}
+
+//========================================
+//TODO - Footer div addEventListener(s)
+function clickable(){
+    document.querySelector('#footSub01').addEventListener('click', ()=>{
+        hide();
+        show("newMessagePage");
+    });
+    document.querySelector('#footSub02').addEventListener('click', ()=>{
+        hide();
+        alert('inbox');
+        show("inbox");
+    });
+    document.querySelector('#footSub03').addEventListener('click', ()=>{
+        hide();
+        alert('outbox');
+        show("outbox");
+    });
 }
 
 
@@ -88,7 +121,8 @@ function makeOutboxGUI(){
 
 
 //========================================
-var arrayMe = ["newMessagePage", "inbox", "outbox"];
+// var arrayMe = ["newMessagePage", "inbox", "outbox"];
+// var arrayMe = ["newMessagePage"];
 function hide(){
         for (var i = 0; i < arrayMe.length; i++) {
             document.getElementById(arrayMe[i]).style.display='none';
